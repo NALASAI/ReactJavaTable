47b12b5a99881642d42b2daae7651cc38e265d9f
HUN-NOTEBOOK-PC\DongHun.KANG <ebyul96428@naver.com>
2024-07-26 00:14:33 +0900

    웹피이지 환경 구축중 프론트엔드 - React프로젝트로 구축 완료 백엔드 - Gradle로 구축중 문제발생 - 수정중

---
+++ BackServer/src/test/java/com/example/BackServer/BackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
