fb66ccdb4f922e28b87ff7ded560db1d1fa266c9
HUN-NOTEBOOK-PC\DongHun.KANG <ebyul96428@naver.com>
2024-08-13 23:17:54 +0900

    헤더 디자인 수정 및 로고 추가

--- blog/src/Page/Header.jsx
+++ blog/src/Page/Header.jsx
@@ -1,25 +1,30 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./Head_Footer.css";
 
 function Header() {
   return (
     <header>
-        <nav>
-            <Link to ="/" className="logo">
-                로고
+        <nav className="head_menu">
+          <div id = "logo_div">
+            <Link to ="/" id="logo">
+                <img src={"/img/DC_inside_logo.png"} id="App-logo" alt="react" />
             </Link>
-            <input type="text" placeholder="갤러리 & 통합검색"/>
+          </div>
+          <input type="text" id="search_page" name="search" autoComplete="off" placeholder="갤러리 & 통합검색"/>
+          {/* onclick시 search의 내용을 서버에 전달하여 내용에 포함된 내용을 가져와 출력 */}
+          <button type="button" id="search_btn">
+            검색
+          </button>
         </nav>
-        <nav>
+        <div className="menu_bar">
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            <li>갤러리</li>
+            <li>마이너갤</li>
+            <li>미니갤</li>
+            <li>갤로그</li>
+            <li>디시뉴스</li>
           </ul>
-        </nav>
+        </div>
     </header>
   );
 }
